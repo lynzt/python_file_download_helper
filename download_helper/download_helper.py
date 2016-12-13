563df390bba8e0f11395a4797505a50270cdb88a
lindsay <techells22@gmail.com>
2016-12-13 14:59:00 -0600

    add get_extension_from_uri

--- download_helper/download_helper.py
+++ download_helper/download_helper.py
@@ -1,13 +1,20 @@
 import urllib
 import os
+# import os.path
+from urlparse import urlparse
 
 def download_file_from_uri(uri, filename):
     testfile = urllib.URLopener()
     testfile.retrieve(uri, filename)
 
+def delete_file(filename):
+    os.remove(filename)
+
+def get_extension_from_uri(uri):
+    url_parsed = urlparse(uri)
+    filename, file_extension = os.path.splitext(url_parsed.path)
+    return file_extension
+
 # return true/false
 def file_exists(filename):
     return os.path.isfile(filename)
-
-def delete_file(filename):
-    os.remove(filename)
--- tests/download_helper_tests.py
+++ tests/download_helper_tests.py
@@ -9,14 +9,17 @@ class UtilsTests(unittest.TestCase):
 
     def test_download_helper_from_uri(self):
         # TODO: fix this - this is a horrible way to test...
-        uri = "http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NYSE&render=download"
-        filename = "tests/files/nyse_companies.csv"
+        uri = "https://yt3.ggpht.com/-p-K2HbhiCfE/AAAAAAAAAAI/AAAAAAAAAAA/k4V1tvBNygo/s100-c-k-no-mo-rj-c0xffffff/photo.jpg"
+        filename = "tests/files/test_pic.jpg"
         download_helper.download_file_from_uri(uri, filename)
 
         self.assertTrue(download_helper.file_exists(filename))
         download_helper.delete_file(filename)
         self.assertFalse(download_helper.file_exists(filename))
 
-
+    def test_get_extension_from_uri(self):
+        uri = "https://yt3.ggpht.com/-p-K2HbhiCfE/AAAAAAAAAAI/AAAAAAAAAAA/k4V1tvBNygo/s100-c-k-no-mo-rj-c0xffffff/photo.jpg"
+        ext = download_helper.get_extension_from_uri(uri)
+        self.assertEqual(ext, '.jpg')
 
 # python -m unittest discover -s tests -p "*_tests.py"
