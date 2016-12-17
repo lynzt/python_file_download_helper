f8c32e9b34574f7ccf7cfad5efe45bad0df2c7cc
lindsay <techells22@gmail.com>
2016-12-17 14:54:08 -0600

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='python_file_download_helper',
-      version='0.0.1',
+      version='0.0.2',
       description='helper functions for downloading a file',
       author='lynzt',
       url='https://github.com/lynzt/python_file_download_helper',
